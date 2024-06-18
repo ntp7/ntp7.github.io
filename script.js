d8dc62faf48f269be0ece7f0e2d784cdb2ef40fe
ntp7 <31138933+ntp7@users.noreply.github.com>
2024-06-18 16:46:03 +0200

    updatemobile

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function showSidebar(){
     const sidebar =document.querySelector('.sidebar')
     sidebar.style.display = 'flex'
 }
+function hideSidebar(){
+    const sidebar =document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}

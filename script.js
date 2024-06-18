8f1340743aa87464fe85c180eec3a7a6ad7c272f
ntp7 <31138933+ntp7@users.noreply.github.com>
2024-06-18 18:09:14 +0200

    coolsidebareh

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function showSidebar(){
     const sidebar =document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
+    sidebar.style.left = "0";
+    sidebar.style.boxShadow = "10px 0 15px -5px rgba(0, 0, 0, 0.6)";
 }
 function hideSidebar(){
     const sidebar =document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
+    sidebar.style.left = "-412px";
+    sidebar.style.boxShadow = "none";
 }
